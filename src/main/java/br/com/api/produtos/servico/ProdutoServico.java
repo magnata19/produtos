a7534003a0f91e35a3aad9630a9e29dcd5c40e84
Davidson <dehpacifico@gmail.com>
2023-12-05 22:50:23 -0300

    Criando metodo para listar todos produtos

--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,5 +1,19 @@
 package br.com.api.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.repositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
   
+  @Autowired
+  private ProdutoRepositorio pr;
+
+  //Método para listar todos produtos
+  public Iterable<ProdutoModelo> listar() {
+    return pr.findAll();
+  }
 }
